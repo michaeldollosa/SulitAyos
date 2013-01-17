0491511295d728b628b17c17dfeb173a57995b1d
michael.dollosa <michael.dollosa@yahoo.com>
2013-01-17 10:04:31 +0800

    users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:product])
+    redirect_to @user
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Samp::Application.routes.draw do
   devise_for :users
   resources :products
+  resources :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
